3e685d0fd2db80d769a84d20ec5ba5cf007a51cb
Ryan Mohta <ryanmohta@gmail.com>
2021-10-14 00:26:55 -0700

    Added all files (don't actually do this when committing to a repo!)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import yams from './yams.jpg';
+import questions from './questions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// our state
+// selectedAnswers: array of 10 elements that each have the selected index
+// submitted: boolean
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedAnswers: new Array(10).fill(undefined),
+      submitted: false
+    };
+  }
+
+  onQuestionSelected = (questionIndex, selectedIndex) => {
+    this.setState(state => {
+      const selectedAnswers = state.selectedAnswers.map((item, index) => {
+        if (index == questionIndex) {
+          return selectedIndex;
+        }
+        else {
+          return item;
+        }
+      });
+      return { selectedAnswers: selectedAnswers };
+    });
+  }
+
+  submit = () => {
+    this.setState({ submitted: true });
+  }
+
+  calculateScore = () => {
+    let score = 0;
+    for (var i = 0; i < this.state.selectedAnswers.length; i++) {
+      if (this.state.selectedAnswers[i] == questions[i].correctAnswerIndex) {
+        score++;
+      }
+    }
+    return score;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <h2>How well do you know about</h2>
+          <h1>YAMS?</h1>
+          <h2>By Ryam</h2>
+        </div>
+        <img className="yam-image" src={yams} alt='An image of yams by ryam'/>
+        <div className="questions">
+          { questions.map((question, index) =>
+              <Question
+                key={ index }
+                questionIndex={ index }
+                question={ question.question }
+                answers={ question.answers }
+                correctAnswerIndex={ question.correctAnswerIndex }
+                onQuestionSelected={ this.onQuestionSelected }
+                submitted={ this.state.submitted }
+              />
+          )}
+        </div>
+        <div className="results">
+          <button onClick={this.submit}>{this.state.submitted ? `${this.calculateScore()}/10 correct!` : "How'd I do?"}</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+// our props
+// question: string
+// answers: [string, string, string, string]
+
+// our state
+// selectedIndex: integer from 0 to 3 OR undefined
+
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedIndex: undefined };
+  }
+
+  getButtonClass(index) {
+    if (this.props.submitted) {
+      if (this.props.correctAnswerIndex == index) {
+        return 'correct';
+      }
+      else if (this.state.selectedIndex == index) {
+        return 'incorrect';
+      }
+    }
+    else if (this.state.selectedIndex == index) {
+      return 'selected';
+    }
+    return '';
+  }
+
+  handleClick(index) {
+    this.setState({ selectedIndex: index });
+    this.props.onQuestionSelected(this.props.questionIndex, index);
+  }
+
+  render() {
+    return (
+      <div className="Question">
+        <h3>{ this.props.question }</h3>
+        <div className="row">
+          { this.props.answers.map((answer, index) =>
+            <button
+              key={ index }
+              className={this.getButtonClass(index)}
+              onClick={ () => this.handleClick(index) }
+            >{ answer }</button>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/questions.js
@@ -0,0 +1,104 @@
+const questions = [
+  {
+    question: "1. What kind of food is a yam?",
+    answers: [
+      "Fruit",
+      "Vegetable",
+      "Mango",
+      "Juice"
+    ],
+    correctAnswerIndex: 1
+  },
+  {
+    question: "2. How many species of yams are found around the world?",
+    answers: [
+      "20",
+      "50",
+      "400",
+      "600"
+    ],
+    correctAnswerIndex: 3
+  },
+  {
+    question: "3. Which continent produces the most yams?",
+    answers: [
+      "Africa",
+      "Asia",
+      "North America",
+      "UC Irvine"
+    ],
+    correctAnswerIndex: 0
+  },
+  {
+    question: "4. Which is these is NOT a possible color of the inside of a yam?",
+    answers: [
+      "White",
+      "Orange",
+      "Yellow",
+      "Purple"
+    ],
+    correctAnswerIndex: 1
+  },
+  {
+    question: "5. What season are yams usually harvested in?",
+    answers: [
+      "Spring",
+      "Summer",
+      "Fall",
+      "Winter"
+    ],
+    correctAnswerIndex: 2
+  },
+  {
+    question: "6. Which vitamin are yams NOT a major source of?",
+    answers: [
+      "Vitamin A",
+      "Vitamin B",
+      "Vitamin C",
+      "Potassium"
+    ],
+    correctAnswerIndex: 0
+  },
+  {
+    question: "7. In Ghana, yams are traditionally served to celebrate what?",
+    answers: [
+      "AntAlmanac and Zotistics joining PeterPortal",
+      "Mars starting work at Cameo",
+      "Ryan stealing Patrick's Igloo",
+      "The arrival of a new baby"
+    ],
+    correctAnswerIndex: 3
+  },
+  {
+    question: "8. What food item are yams usually confused with?",
+    answers: [
+      "Bananas",
+      "Potatoes",
+      "Sweet Potatoes",
+      "Taro"
+    ],
+    correctAnswerIndex: 2
+  },
+  {
+    question: "9. For how long have people been cultivating yams?",
+    answers: [
+      "2,000 years",
+      "10,000 years",
+      "100,000 years",
+      "Since the start of time"
+    ],
+    correctAnswerIndex: 1
+  },
+  {
+    question: "10. Which is NOT a health benefit of yams?",
+    answers: [
+      "Heals skin diseases",
+      "Cures respiratory problems",
+      "Aids digestion",
+      "Improves eyesight"
+    ],
+    correctAnswerIndex: 3
+  },
+];
+
+export default questions;
